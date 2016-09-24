a271d5ecd5d130b48ff2ce425aabbdd834be276f
ownport <ownport@gmail.com>
2016-09-25 01:08:11 +0300

    [*] fixed issue to working with index

--- scrapets/main.py
+++ scrapets/main.py
@@ -28,25 +28,12 @@ def cli():
 
 @cli.command()
 @click.argument('url', nargs=-1)
-@click.option('--urls',
-                type=click.File('rb'),
-                help='fetch files by the list of urls')
-@click.option('--index',
-                type=click.File('rw+'),
-                help='index file for storing metadata')
-@click.option('--path',
-                default=os.getcwd(),
-                type=click.Path(exists=True),
-                help='the target directory, default: %s' % os.getcwd())
-@click.option('--user-agent',
-                default=DEFAULT_USER_AGENT,
-                help='User agent, default: %s' % DEFAULT_USER_AGENT)
-@click.option('--pairtree/--no-pairtree',
-                default=False,
-                help='create pairtree structure in the target directory, default: turn off')
-@click.option('--meta',
-                default='short',
-                help='format or metadata. Possible values: short, detail. Default: short')
+@click.option('--urls', type=click.File('rb'), help='fetch files by the list of urls')
+@click.option('--index', type=click.File('rw+'), help='index file for storing metadata')
+@click.option('--path', default=os.getcwd(), type=click.Path(exists=True), help='the target directory, default: %s' % os.getcwd())
+@click.option('--user-agent', default=DEFAULT_USER_AGENT, help='User agent, default: %s' % DEFAULT_USER_AGENT)
+@click.option('--pairtree/--no-pairtree', default=False, help='create pairtree structure in the target directory, default: turn off')
+@click.option('--meta', default='short', help='format or metadata. Possible values: short, detail. Default: short')
 @click.pass_context
 def fetch(ctx, **opts):
     """ Fetch operations
@@ -72,13 +59,14 @@ def fetch(ctx, **opts):
     elif opts['urls']:
         urls = opts['urls'].readlines()
 
-    urls = [u for u in urls if utils.sha256str(u) not in metadata_index]
+    urls = [u.strip() for u in urls if utils.sha256str(u.strip()) not in metadata_index.keys()]
 
     fetcher = fetch.Fetcher(opts['path'], user_agent=opts['user_agent'])
-    for res in map(lambda u: fetcher.fetch(u.strip(), pairtree=opts['pairtree'], meta=opts['meta']), urls):
-
+    for url in urls:
+        res = fetcher.fetch(url.strip(), pairtree=opts['pairtree'], meta=opts['meta'])
         json_res = json.dumps(res)
-        opts['index'].write("%s\n" % json_res)
         print json_res
+        if opts['index']:
+            opts['index'].write("%s\n" % json_res)
         sha256url = res.pop('url.sha256')
         metadata_index[sha256url] = res
