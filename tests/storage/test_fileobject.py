2fdfa446453561af053b903c77089b1af48288a0
ownport <ownport@gmail.com>
2016-09-24 08:40:46 +0300

    [+] tests/

---
+++ tests/storage/test_fileobject.py
@@ -0,0 +1,120 @@
+
+import os
+import pytest
+
+from scrapets.storage.fileobject import FileObject
+
+def test_fileobject_init(tmpdir):
+
+    path = str(tmpdir.join('fileobject-init'))
+    fo = FileObject(path)
+    assert isinstance(fo, FileObject)
+    assert fo.path == path
+
+
+def test_fileobject_write_str(tmpdir):
+
+    path = str(tmpdir.join('fileobject-write-str'))
+    fo = FileObject(path)
+    fo.write('fileobject-write-str')
+    assert fo.path == path
+
+
+def test_fileobject_write_read_str(tmpdir):
+
+    path = str(tmpdir.join('fileobject-write-read-str'))
+    fo = FileObject(path)
+    fo.write('fileobject-write-read-str')
+    assert fo.path == path
+    assert fo.read() == 'fileobject-write-read-str'
+
+
+def test_fileobject_write_file(tmpdir):
+
+    from cStringIO import StringIO
+    path = str(tmpdir.join('fileobject-write-file'))
+    fo = FileObject(path)
+    fo.write(StringIO('fileobject-write-file'))
+    assert fo.path == path
+
+
+def test_fileobject_meta(tmpdir):
+
+    path = str(tmpdir.join('fileobject-meta'))
+    fo = FileObject(path)
+    fo.write('fileobject-meta-content')
+    assert fo.path == path
+    assert fo.meta == {
+                        'path': path,
+                        'filename': 'fileobject-meta',
+                        'filename/sha256': 'd2088266c0fc064b249d1b9959c80d986cc72b377066ad80e7211837d57e6c09',
+                        'filename/pairtree': 'd2/08/d2088266c0fc064b249d1b9959c80d986cc72b377066ad80e7211837d57e6c09',
+                        'content/sha256': 'e7de0e8d1d59a0f01d7de63a9ed1948f93554bdf63d8f544655b5a89c2e2af1c',
+                        'content/pairtree': 'e7/de/e7de0e8d1d59a0f01d7de63a9ed1948f93554bdf63d8f544655b5a89c2e2af1c',
+                        'content/size': 23,
+    }
+
+def test_fileobject_write_none(tmpdir):
+
+    path = str(tmpdir.join('fileobject-write-none'))
+    fo = FileObject(path)
+    with pytest.raises(RuntimeError):
+        fo.write(None)
+
+
+def test_fileobject_rename(tmpdir):
+
+    path = tmpdir.join('fileobject-rename')
+    fo = FileObject(str(path))
+    fo.write('fileobject-rename')
+    assert fo.path == path
+
+    new_path = os.path.join(path.dirname, 'fileobject-rename-new')
+    fo.rename(new_path)
+    assert fo.read() == 'fileobject-rename'
+
+
+def test_fileobject_tmppath_no_file(tmpdir):
+
+    path = tmpdir.join('fileobject-tmppath-no-file')
+    fo = FileObject(str(path))
+    assert fo.path == path
+
+    fo.mktemp()
+    assert fo.path.endswith('fileobject-tmppath-no-file.part')
+    assert not os.path.exists(fo.path)
+
+    fo.rmtemp()
+    assert fo.path.endswith('fileobject-tmppath-no-file')
+    assert not os.path.exists(fo.path)
+
+
+def test_fileobject_tmppath_with_file(tmpdir):
+
+    path = tmpdir.join('fileobject-tmppath-with-file')
+    fo = FileObject(str(path))
+    assert fo.path == path
+
+    fo.write('fileobject-tmppath-with-file')
+    assert fo.read() == 'fileobject-tmppath-with-file'
+
+    fo.mktemp()
+    assert fo.path.endswith('fileobject-tmppath-with-file.part')
+    assert os.path.exists(fo.path)
+
+    fo.rmtemp()
+    assert fo.path.endswith('fileobject-tmppath-with-file')
+    assert os.path.exists(fo.path)
+
+
+def test_fileobject_tmppath_two_files_with_same_name(tmpdir):
+
+    path1 = tmpdir.join('fileobject-tmppath-twofiles')
+    fo1 = FileObject(str(path1))
+    fo1.write('fileobject-tmppath-twofiles-1')
+    assert fo1.read() == 'fileobject-tmppath-twofiles-1'
+
+    path2 = tmpdir.join('fileobject-tmppath-twofiles')
+    fo2 = FileObject(str(path2))
+    fo2.write('fileobject-tmppath-twofiles-2')
+    assert fo2.read() == 'fileobject-tmppath-twofiles-2'
---
+++ tests/test_fetch.py
@@ -0,0 +1,90 @@
+
+import os
+import pytest
+
+from StringIO import StringIO
+
+from scrapets.fetch import Fetcher
+from scrapets.errors import PathDoesNotExist
+
+
+SUCCESS_URL = 'http://example.com'
+NONE_URL = 'http://none.com'
+
+RESULT_STATUS_200 = {
+    'url': 'http://example.com',
+    'url/sha256': 'f0e6a6a97042a4f1f1c87f5f7d44315b2d852c2df5c7991cc66241bf7072d1c4',
+    'url/pairtree': 'f0/e6/f0e6a6a97042a4f1f1c87f5f7d44315b2d852c2df5c7991cc66241bf7072d1c4',
+    'file/sha256': '9a9b3e8cb888d070aff71af3e08626005ebb948266a7a97dbacbd054d7231046',
+    'file/pairtree': '9a/9b/9a9b3e8cb888d070aff71af3e08626005ebb948266a7a97dbacbd054d7231046'
+}
+
+
+class MockedRequest(object):
+
+    def __init__(self, url, headers):
+
+        self._url = url
+        self._headers = headers
+
+    def get(self):
+
+        return self
+
+    def send(self, stream=False):
+
+        if self._url == SUCCESS_URL:
+            return MockedResponse(code=200, body=StringIO('MockedResponse200'))
+        elif self._url == NONE_URL:
+            return MockedResponse(code=404, body=StringIO('MockedResponse404'))
+
+
+class MockedResponse(object):
+
+    def __init__(self, code, body):
+
+        self.code = code
+        self.body = body
+
+
+def test_fetch_create(tmpdir):
+
+    path = str(tmpdir)
+    fetcher = Fetcher(path)
+    assert isinstance(fetcher, Fetcher)
+    assert fetcher.path == path
+
+
+def test_fetch_path_does_not_exist():
+
+    with pytest.raises(PathDoesNotExist):
+        fetcher = Fetcher('the-path-does-not-exist')
+
+
+def test_fetch_single_url(tmpdir, monkeypatch):
+
+    monkeypatch.setattr("scrapets.packages.reqres.Request", lambda url, headers: MockedRequest(url, headers))
+    path = str(tmpdir.mkdir('single-url'))
+    fetcher = Fetcher(path)
+    assert fetcher.fetch(SUCCESS_URL) == (RESULT_STATUS_200,)
+
+
+def test_fetch_multiple_urls(tmpdir, monkeypatch):
+
+    monkeypatch.setattr("scrapets.packages.reqres.Request", lambda url, headers: MockedRequest(url, headers))
+    path = str(tmpdir.mkdir('multiple-urls'))
+    fetcher = Fetcher(path)
+    assert fetcher.path == path
+
+    urls = [SUCCESS_URL for i in range(3)]
+    for r in fetcher.fetch(urls):
+        assert r == RESULT_STATUS_200
+
+
+def test_fetch_return_error_result(tmpdir, monkeypatch):
+
+    monkeypatch.setattr("scrapets.packages.reqres.Request", lambda url, headers: MockedRequest(url, headers))
+    path = str(tmpdir.mkdir('error-result'))
+    fetcher = Fetcher(path)
+    assert fetcher.path == path
+    assert fetcher.fetch(NONE_URL) == ()
