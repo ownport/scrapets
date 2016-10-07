e581709937419f4fae250aa3ee039f57eb5d6bd1
ownport <ownport@gmail.com>
2016-10-07 07:45:02 +0300

    [+] internal packages support

--- scrapets/__init__.py
+++ scrapets/__init__.py
@@ -4,9 +4,6 @@ __version__ = "0.1.1"
 import os
 import sys
 
-if __package__ is None :
-    path = os.path.realpath(os.path.abspath(__file__))
-    sys.path.insert(0, os.path.dirname(os.path.dirname(path)))
-
-# path = os.path.realpath(os.path.abspath(__file__))
-# sys.path.insert(0, os.path.join(os.path.dirname(path), 'packages/'))
+path = os.path.realpath(os.path.abspath(__file__))
+sys.path.insert(0, os.path.dirname(os.path.dirname(path)))
+sys.path.insert(0, os.path.join(os.path.dirname(path), 'packages/'))
